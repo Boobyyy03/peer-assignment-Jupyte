976a790e16168161e4eec3e9585d49c95c57686c
Trần Minh Hoàng <108820186+Boobyyy03@users.noreply.github.com>
2023-09-18 13:55:44 +0700

    Add files via upload

---
+++ peer assignment jupyter.py
@@ -0,0 +1,72 @@
+# Data Science Notebook
+
+
+## Introduction
+
+Welcome to this Data Science notebook! In this notebook, we will explore various data science concepts, languages, libraries, and tools. We'll also dive into some practical examples and exercises to enhance your understanding of data science.
+
+Let's get started!
+
+
+## Data Science Languages
+
+- Python
+- R
+- Julia
+- SQL
+- Scala
+
+## Data Science Libraries
+
+- Pandas
+- NumPy
+- Scikit-learn
+- TensorFlow
+- Keras
+- Matplotlib
+- Seaborn
+- ggplot2
+
+
+
+## Data Science Tools
+
+| Tool           | Description                                |
+|----------------|--------------------------------------------|
+| Jupyter Notebook | Interactive computing environment         |
+| RStudio        | Integrated development environment for R   |
+| Spyder         | Scientific Python development environment  |
+| VSCode         | Code editor with data science extensions   |
+| Tableau        | Data visualization and business intelligence|
+| TensorFlow     | Open-source machine learning framework     |
+| Power BI       | Business analytics and data visualization  |
+
+
+
+## Arithmetic Expressions
+
+In this section, we'll explore some basic arithmetic expressions and mathematical operations.
+
+
+# Multiply and add numbers
+result = 5 * 3 + 10
+result
+
+
+# Convert minutes to hours
+minutes = 120
+hours = minutes / 60
+hours
+
+
+## Objectives
+
+- Explore data science languages and libraries.
+- Understand the role of data science tools.
+- Practice basic arithmetic expressions.
+- Perform simple calculations and conversions.
+
+
+## Author
+
+- Hoàng Trần 
